e2e25fed3bbdb7c75d246ee994601e974cebb11f
mileszrobi <ubuntu@ip-172-31-29-255.us-west-2.compute.internal>
2013-07-15 18:19:54 +0000

    web.js reads index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var message = fs.readFileSync('index.html').toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
